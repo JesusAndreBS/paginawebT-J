b4cdbd6b373feb412a0532760337f052dc7aeb67
JesúsAndreBS <jesusandrebautistasanchez556@gmail.com>
2026-03-24 10:16:04 -0500

    correccion de codigo1

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 // servir archivos estáticos
-app.use(express.static("paginaweb"));
-
+app.use(express.static("../../frontend"));
+""
 app.listen(3000, () => {
   console.log("Servidor en http://localhost:3000");
 });
